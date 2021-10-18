059a6317bf86194ae6d155b9084ece73120d587e
jeffgallagher86 <jeffgallagher86@gmail.com>
2021-10-18 15:23:08 +0000

    updated products fixtures json files and created database

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, BreweryCategory, StyleCategory, CountryCategory
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(BreweryCategory)
+admin.site.register(StyleCategory)
+admin.site.register(CountryCategory)
--- products/models.py
+++ products/models.py
@@ -35,15 +35,15 @@ class CountryCategory(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=254)
     sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
     description = models.TextField()
     brewery = models.ForeignKey('BreweryCategory', null=True, blank=True, on_delete=models.SET_NULL)
     style = models.ForeignKey('StyleCategory', null=True, blank=True, on_delete=models.SET_NULL)
     country = models.ForeignKey('CountryCategory', null=True, blank=True, on_delete=models.SET_NULL)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    size = models.DecimalField(max_digits=6)
-    abv = models.DecimalField(max_digits=6, decimal_places=2)
+    size = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    abv = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
