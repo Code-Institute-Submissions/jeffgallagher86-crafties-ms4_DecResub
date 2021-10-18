fdc12642ffb4dd6325ba5beb4bd48c819adc8216
jeffgallagher86 <jeffgallagher86@gmail.com>
2021-10-18 12:02:49 +0000

    Added fixture files and product models

---
+++ products/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+
+class BreweryCategory(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class StyleCategory(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class CountryCategory(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField()
+    brewery = models.ForeignKey('BreweryCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    style = models.ForeignKey('StyleCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    country = models.ForeignKey('CountryCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    size = models.DecimalField(max_digits=6)
+    abv = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
