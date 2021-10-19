72b0d535a317801ad3c0e21d2d91a68b141c2861
jeffgallagher86 <jeffgallagher86@gmail.com>
2021-10-19 20:22:26 +0000

    Added category filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, BreweryCategory, StyleCategory, CountryCategory
 
 # Create your views here.
 
@@ -11,8 +11,26 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    breweries = None
+    styles = None
+    countries = None
 
     if request.GET:
+        if 'brewery' in request.GET:
+            breweries = request.GET['brewery'].split(',')
+            products = products.filter(brewery__name__in=breweries)
+            breweries = BreweryCategory.objects.filter(name__in=breweries)
+
+        if 'style' in request.GET:
+            styles = request.GET['style'].split(',')
+            products = products.filter(style__name__in=styles)
+            styles = StyleCategory.objects.filter(name__in=styles)
+
+        if 'country' in request.GET:
+            countries = request.GET['country'].split(',')
+            products = products.filter(country__name__in=countries)
+            countries = CountryCategory.objects.filter(name__in=countries)
+    
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -25,6 +43,9 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_breweries': breweries,
+        'current_styles': styles,
+        'current_counries': countries,
     }
 
     return render(request, 'products/products.html', context)
