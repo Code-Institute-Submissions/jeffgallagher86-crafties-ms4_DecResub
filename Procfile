692c00e28d4acabc1922778f476acbdabd1663d7
jeffgallagher86 <jeffgallagher86@gmail.com>
2021-10-25 22:26:19 +0000

    Further heroku deployment setting added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn crafties-ms4-jg.wsgi:application
--- crafties/settings.py
+++ crafties/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-r#1v-mndur44zybp7ki_9k86*(b0!s9%nhuzn1gm3^j8ry_23p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['crafties-ms4-jg.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -106,12 +106,17 @@ WSGI_APPLICATION = 'crafties.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
