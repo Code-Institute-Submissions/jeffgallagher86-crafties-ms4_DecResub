d7b0b42b61b72d4bf9f7fba91f58132e9f6cf9d0
jeffgallagher86 <jeffgallagher86@gmail.com>
2021-10-27 11:56:31 +0000

    updated stripe API settings

--- crafties/settings.py
+++ crafties/settings.py
@@ -204,6 +204,9 @@ if 'USE_AWS' in os.environ:
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
